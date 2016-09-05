329768faf9d9f741c01656bcb84902d819067418
Mike Barlow <mike@mikebarlow.co.uk>
2016-09-05 22:42:02 +0000

    Added constructors for event and calendar objects

--- src/Calendar.php
+++ src/Calendar.php
@@ -1,7 +1,25 @@
 <?php
 namespace Snscripts\MyCal;
 
+use Snscripts\MyCal\Interfaces\CalendarInterface;
+use Snscripts\MyCal\Interfaces\EventInterface;
+
 class Calendar
 {
-    
+    protected $calendarProvider;
+    protected $eventProvider;
+
+    /**
+     * Setup a new calendar object
+     *
+     * @param CalendarInterface $calendarProvider
+     * @param EventInterface $eventProvider
+     */
+    public function __construct(
+        CalendarInterface $calendarProvider,
+        EventInterface $eventProvider
+    ) {
+        $this->calendarProvider = $calendarProvider;
+        $this->$eventProvider = $eventProvider;
+    }
 }
--- src/Event.php
+++ src/Event.php
@@ -3,5 +3,15 @@ namespace Snscripts\MyCal;
 
 class Event
 {
+    protected $eventProvider;
 
+    /**
+     * Setup a new Event Object
+     *
+     * @param EventInterface $eventProvider
+     */
+    public function __construct(EventInterface $eventProvider)
+    {
+        $this->$eventProvider = $eventProvider;
+    }
 }
