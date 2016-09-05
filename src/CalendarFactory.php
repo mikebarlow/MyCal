4860c048d78f46d212a15cda20cfefbf3c53fde1
Mike Barlow <mike@mikebarlow.co.uk>
2016-09-05 22:39:49 +0000

    Removed duplicate eloquent models cause i spelt it wrong and created calendar factory

--- src/CalendarFactory.php
+++ src/CalendarFactory.php
@@ -1,8 +1,39 @@
 <?php
 namespace Snscripts\MyCal;
 
+use Snscripts\MyCal\Interfaces\CalendarInterface;
+use Snscripts\MyCal\Interfaces\EventInterface;
+use Snscripts\MyCal\Calendar;
+
 class CalendarFactory
 {
+    protected $calendarProvider;
+    protected $eventProvider;
 
-    
+    /**
+     * Setup a new calendar factory with these providers
+     *
+     * @param CalendarInterface $calendarProvider
+     * @param EventInterface $eventProvider
+     */
+    public function __construct(
+        CalendarInterface $calendarProvider,
+        EventInterface $eventProvider
+    ) {
+        $this->calendarProvider = $calendarProvider;
+        $this->$eventProvider = $eventProvider;
+    }
+
+    /**
+     * create a new calendar instance
+     *
+     * @return Calendar $Calendar
+     */
+    public function newInstance()
+    {
+        return new Calendar(
+            $this->calendarProvider,
+            $this->eventProvider
+        );
+    }
 }
