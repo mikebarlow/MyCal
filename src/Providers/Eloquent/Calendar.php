da0b193bfdf927715c749286c0352be7249af2b2
Mike Barlow <mike@mikebarlow.co.uk>
2016-09-06 22:53:03 +0000

    Started working on the calendar provider to save a calendar

--- src/Providers/Eloquent/Calendar.php
+++ src/Providers/Eloquent/Calendar.php
@@ -2,10 +2,17 @@
 namespace Snscripts\MyCal\Providers\Eloquent;
 
 use Snscripts\MyCal\Interfaces\CalendarInterface;
+use Aura\Payload_Interface\PayloadStatus;
 
 class Calendar implements CalendarInterface
 {
     protected $model = 'Snscripts\Providers\Eloquent\Models\Calendar';
+
+    protected $fields = [
+        'id',
+        'name',
+        'is_active'
+    ];
 
     /**
      * Save a calendar
@@ -15,5 +22,31 @@ class Calendar implements CalendarInterface
      */
     public function save($Calendar)
     {
+        $Model = new $this->model;
+
+        foreach ($this->fields as $field) {
+            if (isset($Calendar->{$field})) {
+                $Model->{$field} = $Calendar->{$field};
+            }
+        }
+
+        // todo: validation?
+
+        $PayloadFactory = new \Aura\Payload\PayloadFactory;
+        $Payload = $PayloadFactory->newInstance();
+
+        try {
+            if ($Model->save()) {
+                $Payload->setStatus(PayloadStatus::SUCCESS);
+            } else {
+                $Payload->setStatus(PayloadStatus::FAILURE);
+            }
+        } catch (\Exception $e) {
+            $Payload->setStatus(PayloadStatus::ERROR)
+                ->setMessages($e->getMessage())
+                ->setInput($Model);
+        }
+
+        return $Payload;
     }
 }
