a96f96940778787d1236e41f54dc938265cb7871
Mike Barlow <mike@mikebarlow.co.uk>
2016-10-23 18:36:33 +0000

    display method tests

--- tests/Calendar/DateTest.php
+++ tests/Calendar/DateTest.php
@@ -6,6 +6,11 @@ use DateTimeZone;
 
 class DateTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        date_default_timezone_set('UTC');
+    }
+
     public function testCanCreateInstance()
     {
         $this->assertInstanceOf(
@@ -15,6 +20,32 @@ class DateTest extends \PHPUnit_Framework_TestCase
                 new DateTimeZone('Europe/London'),
                 Date::MONDAY
             )
+        );
+    }
+
+    public function testDisplayMethodCorrectlyOutputsDate()
+    {
+        $time = mktime('12', '00', '00', '10', '23', '2016');
+        $Date = new Date(
+            $time,
+            new DateTimeZone('Europe/London'),
+            Date::MONDAY
+        );
+
+        $this->assertSame(
+            '2016-10-23 13:00:00',
+            $Date->display('Y-m-d H:i:s')
+        );
+
+        $Date = new Date(
+            $time,
+            new DateTimeZone('America/New_York'),
+            Date::MONDAY
+        );
+
+        $this->assertSame(
+            '2016-10-23 08:00:00',
+            $Date->display('Y-m-d H:i:s')
         );
     }
 }
