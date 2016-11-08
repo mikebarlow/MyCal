ce3022779bdfb62f1959e7e7ada50ddeecf94ac5
Mike Barlow <mike@mikebarlow.co.uk>
2016-11-08 14:06:47 +0000

    starting build method

--- src/Calendar/Calendar.php
+++ src/Calendar/Calendar.php
@@ -29,4 +29,41 @@ class Calendar
         $this->dateFactory = $dateFactory;
         $this->Options = $Options;
     }
+
+    /**
+     * Get a collection of dates inclusive of given dates
+     *
+     * @todo tidy this up
+     * @param date $start Start date to get Y-m-d format
+     * @param date $end End date to get Y-m-d format
+     * @return \Cartalyst\Collections\Collection
+     */
+    public function build($start, $end)
+    {
+        $dates = [];
+
+        $startDate = new \DateTime($start);
+        $endDate = new \DateTime($end);
+
+        $interval = new \DateInterval('P1D');
+        $range = new \DatePeriod(
+            $startDate,
+            $interval,
+            $endDate
+        );
+
+        $DateTimeZone = new DateTimeZone(
+            $this->Options->defaultTimezone
+        );
+
+        foreach ($range as $date) {
+            $dates[] = $this->dateFactory->newInstance(
+                $date->getTimestamp(),
+                $DateTimeZone,
+                $this->Options->weekStart
+            );
+        }
+
+        return new \Cartalyst\Collections\Collection($dates);
+    }
 }
