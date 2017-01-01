16a12f3215b8eedad6d65a7e9d3194c5327bc9eb
Mike Barlow <mike@mikebarlow.co.uk>
2017-01-01 15:17:13 +0000

    Ignore files from some tests

--- src/Integrations/Eloquent/Models/CalendarExtra.php
+++ src/Integrations/Eloquent/Models/CalendarExtra.php
@@ -3,6 +3,9 @@ namespace Snscripts\MyCal\Integrations\Eloquent\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @codeCoverageIgnore
+ */
 class CalendarExtra extends Model
 {
     public $incrementing = false;
--- src/Integrations/Eloquent/MyCalServiceProvider.php
+++ src/Integrations/Eloquent/MyCalServiceProvider.php
@@ -4,6 +4,9 @@ namespace Snscripts\MyCal\Integrations\Eloquent;
 
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * @codeCoverageIgnore
+ */
 class MyCalServiceProvider extends ServiceProvider
 {
     public function register()
